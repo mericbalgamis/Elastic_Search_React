8b400b40ab7487b46c0b594d19df44f51e8e32bd
Eda Balaban <edabalabannn>
2019-07-17 17:36:47 +0300

    Generic form json hatası düzeltildi. Http get metodu ile istek atılması sağlandı.Gelen response result panelde gösterildi.Json beautify vetema ayarlamaları yapıldı.

---
+++ build/precache-manifest.596eded0d0002c9121b472314f9b2df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd070d92861cce4d8e749249c7797e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383d78fe5b9686fee568",
+    "url": "/static/css/2.600dfb7d.chunk.css"
+  },
+  {
+    "revision": "96dbdc3d382ea2b9a6eb",
+    "url": "/static/css/main.1dbfa5d1.chunk.css"
+  },
+  {
+    "revision": "383d78fe5b9686fee568",
+    "url": "/static/js/2.edb99409.chunk.js"
+  },
+  {
+    "revision": "96dbdc3d382ea2b9a6eb",
+    "url": "/static/js/main.973aeba8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b27047938dba72ed16cc384fd20863c",
+    "url": "/static/media/datatypes.8b270479.txt"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import DynamicForm from './components/dynamicForm/dynamicForm';
 import myJson from './datatypes.json';
 import txt from './datatypes.txt';
 import json from './config/simple.json';
+import axios from 'axios';
+import JSONPretty from 'react-json-prettify';
+import {github} from 'react-json-prettify/dist/themes';
+
 
 
 var formJson = {
@@ -17,100 +21,129 @@ var formJson = {
 class App extends Component {
 
   constructor(props) {
+
     super(props);
-    this.state = {form: formJson};
+    this.state = {
 
-  };
+      form: formJson,
+      response: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-
-submitHandler = (event) => {
-  let json = JSON.stringify(event, null, 4);
-  console.log(json);
-}
-
-componentWillMount() {
-
-  fetch(txt)
-    .then((r) => r.text())
-    .then(text => {
-      let myarray = text.split('\n');
-
-      myarray.map((element, i) => {
-
-        var array = element.split(' ');
-
-        if (array.length === 4) {
-
-          let name = array[0];
-          name = name.replace(/_/g, ' ');
-          let type = array[1];
-          let options = array[2];
-          options = options.replace(/\)/g, '');
-          options = options.replace(/\(/g, '');
-          let optionsArray = options.split(',');
-
-          var optionData = { "options": [] };
-          var data = {};
-          optionsArray.map((option, i) => {
-            optionData.options.push({ "display": option });
-          })
-
-          data = {
-            "id": name,
-            "label": name,
-            "description": "",
-            "type": type,
-            "value": "",
-            "required": "false",
-            "placeholder": "",
-            "definition": optionData.options
-          };
-        }
-
-        else if (array.length === 3) {
-          let name = array[0];
-          name = name.replace(/_/g, ' ');
-          let type = array[1];
-
-          data = {
-            "id": name,
-            "label": name,
-            "description": "",
-            "type": type,
-            "value": "",
-            "required": "false",
-            "placeholder": ""
-          };
-        }
-
-        console.log(json)
-        this.setState({form : this.state.form.fields.push(data)});
-        //console.log(formJson);
-
+  handleClick() {
+    axios.get()
+      .then(res => {
+        this.setState({
+          todo: res.data
+        })
       })
+  }
 
-      //this.setState({form : formJson});
-    })
-}
+  sendRequest() {
+    console.log("handleclick")
+    var panel=document.getElementById("resultPanel");
+    axios.get('http://127.0.0.1:5000/query')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          response: response.data
+        })
+        //panel.innerHTML=response.data; 
+      });
+  }
 
-render() {
-  return (
-    <div className="App">
 
-      <SplitterLayout>
-        <div>Build a Query !
+  submitHandler = (event) => {
+    let json = JSON.stringify(event, null, 4);
+    console.log(json);
+  }
+
+  componentWillMount() {
+
+    fetch(txt)
+      .then((r) => r.text())
+      .then(text => {
+        let myarray = text.split('\n');
+        let dataArray = myarray.map((element, i) => {
+
+
+          var array = element.split(' ');
+
+          if (array.length === 4) {
+
+            let name = array[0];
+            name = name.replace(/_/g, ' ');
+            let type = array[1];
+            let options = array[2];
+            options = options.replace(/\)/g, '');
+            options = options.replace(/\(/g, '');
+            let optionsArray = options.split(',');
+
+            var optionData = { "options": [] };
+            var data = {};
+            optionsArray.map((option, i) => {
+              optionData.options.push({ "display": option });
+            })
+
+            data = {
+              "id": name,
+              "label": name,
+              "description": "",
+              "type": type,
+              "value": "",
+              "required": "false",
+              "placeholder": "",
+              "definition": optionData.options
+            };
+          }
+
+          else if (array.length === 3) {
+            let name = array[0];
+            name = name.replace(/_/g, ' ');
+            let type = array[1];
+
+            return {
+              "id": name,
+              "label": name,
+              "description": "",
+              "type": type,
+              "value": "",
+              "required": "false",
+              "placeholder": ""
+            };
+          }
+
+        })
+        this.setState({
+          form: {
+            fields: dataArray
+          }
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <SplitterLayout>
+          <div>Build a Query !
             <DynamicForm id="form" config={this.state.form} onSubmit={(event) => this.submitHandler(event)}></DynamicForm>
-        </div>
-        <div>
-          <Panel>
-            {"sdknsdjsk"}
-          </Panel>
-        </div>
-      </SplitterLayout>
 
-    </div>
-  );
-}
+          </div>
+
+
+          <div>
+          <button onClick={() => this.sendRequest()}>baz</button>
+
+            <Panel id="resultPanel"><JSONPretty json={this.state.response} theme={github} /></Panel>
+          </div>
+        </SplitterLayout>
+
+      </div>
+    );
+  }
 }
 
 export default App;
